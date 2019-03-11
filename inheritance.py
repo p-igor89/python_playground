e30e4f8b9732480eae5462b5ff996cff18889e0b
p-igor89 <iperetyatko@techincubator.com.au>
2019-03-11 02:11:53 +0200

    (DEV) add some inheritance

---
+++ inheritance.py
@@ -0,0 +1,20 @@
+class Parent(object):
+    def __init__(self):
+        print('Parent inited')
+        self.value = 'Parent'
+
+    def do(self):
+        print('Parent do(): {}'.format(self.value))
+
+
+class Child(Parent):
+    def __init__(self):
+        print('Child inited')
+        self.value = 'Child'
+
+
+parent = Parent()
+parent.do()
+
+child = Child()
+child.do()
