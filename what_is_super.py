2a23a0c759f27ae991ff40a91af8a1a1203c535d
p-igor89 <iperetyatko@techincubator.com.au>
2019-03-11 02:53:24 +0200

    (DEV) add some what is super

---
+++ what_is_super.py
@@ -0,0 +1,28 @@
+class Calc(object):
+    def __init__(self, value):
+        print('Calc constructor is called')
+        self.value = value
+
+    def count(self):
+        return self.value * 9 + 7
+
+
+c = Calc(1.4)
+print(c.count())
+
+
+class ExtendedCalc(Calc):
+    def __init__(self, value, k=1):
+        super().__init__(value)
+        print('Extender', self.value)
+
+        self.k = k
+
+    def count(self):
+        print('Before')
+        previous = super().count()
+        return -1 * self.k * previous
+
+
+e = ExtendedCalc(8, k=1.2)
+print(e.count())
